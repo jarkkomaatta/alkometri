e1d5e56b5422b5dd33f17c1bf10e7f2f9c96d75f
jarkkomaatta <127930482+jarkkomaatta@users.noreply.github.com>
2023-09-07 11:00:26 +0300

    pudotuslista muokkaus

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [time, setTime] = useState(0)
   const [gender, setGender] = useState(0)
   const [result, setResult] = useState(0)
+  const bottlesNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  const timeNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 
   function handleSubmit(e) {
@@ -37,53 +39,21 @@ function App() {
         <div>
           <label for="bottles">Bottles</label>
           <select name="bottles" id="bottles" value={bottles} onChange={e => setBottles(e.target.value)}>
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
+            {
+              bottlesNumbers.map(bottle => (
+                <option value={bottle}>{bottle} pulloa</option>
+              ))
+            }
           </select>
         </div>
         <div>
           <label for="time">Time</label>
           <select name="time" id="time" value={time} onChange={e => setTime(e.target.value)}>
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
+            {
+              timeNumbers.map(time => (
+                <option value = {time}>{time} tuntia</option>
+              ))
+            }
           </select>
         </div>
         <div>
